f3cc7615f2ab85e27e1622a3b3dfb6a812eae3a9
Wojciech Polnik <wojciech.polnik@zerodowntime.pl>
2019-12-01 01:40:48 +0100

    updated script

--- ansible-vault-check.sh
+++ ansible-vault-check.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Pre-commit hook that verifies if all files containing 'vault' in the name
 # are encrypted.
@@ -17,28 +17,23 @@ yellow='\033[1;33m'
 # carriage return hack. Leave it on 2 lines.
 cr='
 '
-# for f in $(git diff --cached --name-only | grep -E $FILES_PATTERN)
-# do
+for f in $@
+do
 # test for the presence of the required bit.
-MATCH=`head -n1 $f | grep --no-messages $REQUIRED`
-if [ ! $MATCH ] ; then
-  # Build the list of unencrypted files if any
-  UNENCRYPTED_FILES="$f$cr$UNENCRYPTED_FILES"
-  EXIT_STATUS=1
-fi
-# done
+  MATCH=`head -n1 $f | grep --no-messages $REQUIRED`
+  if [ ! $MATCH ] ; then
+    # Build the list of unencrypted files if any
+    UNENCRYPTED_FILES="$f$cr$UNENCRYPTED_FILES"
+    EXIT_STATUS=1
+  fi
+done
 if [ ! $EXIT_STATUS = 0 ] ; then
-  echo '# COMMIT REJECTED'
-  echo '# Looks like unencrypted ansible-vault files are part of the commit:'
-  echo '#'
   while read -r line; do
     if [ -n "$line" ]; then
       echo -e "#\t${yellow}unencrypted:   $line${wipe}"
     fi
   done <<< "$UNENCRYPTED_FILES"
-  echo '#'
   echo "# Please encrypt them with 'ansible-vault encrypt <file>'"
-  echo "#   (or force the commit with '--no-verify')."
   exit $EXIT_STATUS
 fi
 exit $EXIT_STATUS
